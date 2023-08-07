5fe35d6b808cf48adba6bb98a2c9d8d9bfc0619d
VyacheslavMusiienko <musiienko.viacheslav1@gmail.com>
2023-08-07 18:18:49 +0300

    feat: deploy in netlify

--- src/helpers/constant.ts
+++ src/helpers/constant.ts
@@ -1,7 +1,7 @@
 export const DISPLAY_MODE_MONTH = 'month';
 export const DISPLAY_MODE_DAY = 'day';
 
-export const URI = 'http://localhost:3000';
+export const URI = process.env.API_URI ? process.env.API_URI : 'http://localhost:3000';
 export const TOTAL_DAY = 42;
 
 export const ITEMS_TIME_DAY = 24;
