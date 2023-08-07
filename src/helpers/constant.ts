bb66cd32c57a7a651e41041dd2b7e46af3472bd3
VyacheslavMusiienko <musiienko.viacheslav1@gmail.com>
2023-08-07 19:47:53 +0300

    fix: uri

--- src/helpers/constant.ts
+++ src/helpers/constant.ts
@@ -1,8 +1,8 @@
 export const DISPLAY_MODE_MONTH = 'month';
 export const DISPLAY_MODE_DAY = 'day';
 
-export const URI = process.env.API_URI || 'http://localhost:3000';
-// export const URI = 'https://calenadr-json-server-8e6fd1aad9fa.herokuapp.com';
+// export const URI = process.env.API_URI || 'http://localhost:3000';
+export const URI = 'https://calenadr-json-server-8e6fd1aad9fa.herokuapp.com';
 export const TOTAL_DAY = 42;
 
 export const ITEMS_TIME_DAY = 24;
